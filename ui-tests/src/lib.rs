e633be7b6084dcc87bbffd2e1362fad76f63a53a
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2026-01-24 14:34:45 +0530

    only run ui-tests on rustc <= 1.72

--- ui-tests/src/lib.rs
+++ ui-tests/src/lib.rs
@@ -6,7 +6,7 @@ fn ui() {
     let fail = trybuild::TestCases::new();
     if version.minor <= 65 {
         fail.compile_fail("fail-1.65/*.rs");
-    } else {
+    } else if version.minor <= 72 {
         fail.compile_fail("fail-1.72/*.rs");
     }
 }
